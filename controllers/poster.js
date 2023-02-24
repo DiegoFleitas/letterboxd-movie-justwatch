d6bf2b6765abc917ff5d98bf0426ab2e2b7b6b2a
Diego Fleitas <diego.fleitas68@gmail.com>
2023-02-23 21:59:31 -0300

    fix undefined cacheTtl

--- controllers/poster.js
+++ controllers/poster.js
@@ -1,5 +1,6 @@
 const axios = require("../helpers/axios");
 const { getCacheValue, setCacheValue } = require("../helpers/redis");
+const cacheTtl = process.env.CACHE_TTL || 60; // seconds
 
 const poster = async (req, res) => {
   const omdbApiKey = process.env.OMDB_API_KEY;
