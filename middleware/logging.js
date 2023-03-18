2230d741200d51927a418d93282a8fd9a73039cc
Diego Fleitas <diego.fleitas68@gmail.com>
2023-03-18 11:27:47 -0300

    set up logging middleware

---
+++ middleware/logging.js
@@ -0,0 +1,45 @@
+const morgan = require("morgan");
+const uaParser = require("ua-parser-js");
+
+morgan.token("browser", function (req) {
+  const userAgent = req.headers["user-agent"];
+  const ua = uaParser(userAgent);
+  return `${ua.browser.name} ${ua.browser.version}`;
+});
+
+morgan.token("os", function (req) {
+  const userAgent = req.headers["user-agent"];
+  const ua = uaParser(userAgent);
+  return `${ua.os.name} ${ua.os.version}`;
+});
+
+morgan.token("bot", function (req) {
+  const userAgent = req.headers["user-agent"];
+  const ua = uaParser(userAgent);
+  return ua.device.type === "bot" ? "yes" : "no";
+});
+
+// Define a custom morgan format to log JSON to the client
+morgan.format("json", function (tokens, req, res) {
+  if (req.headers["user-agent"] === "Consul Health Check") {
+    return;
+  }
+  const logLevel = res.statusCode >= 400 ? "error" : "info";
+  return `[${logLevel}] ${JSON.stringify(
+    {
+      ip: tokens["remote-addr"](req, res),
+      userAgent: req.headers["user-agent"],
+      browser: tokens.browser(req),
+      os: tokens.os(req),
+      isBot: tokens.bot(req),
+      method: tokens.method(req, res),
+      url: tokens.url(req, res),
+      status: tokens.status(req, res),
+      payload: JSON.stringify(req.body),
+      contentType: req.headers["content-type"],
+      responseTime: tokens["response-time"](req, res),
+    },
+    null,
+    2
+  )}`;
+});
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const morgan = require("morgan");
-const uaParser = require("ua-parser-js");
+require("./middleware/logging");
 require("dotenv").config();
 const {
   searchMovie,
@@ -16,50 +16,7 @@ const { isHealthy } = require("./helpers/redis");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static("public"));
-
-morgan.token("browser", function (req) {
-  const userAgent = req.headers["user-agent"];
-  const ua = uaParser(userAgent);
-  return `${ua.browser.name} ${ua.browser.version}`;
-});
-
-morgan.token("os", function (req) {
-  const userAgent = req.headers["user-agent"];
-  const ua = uaParser(userAgent);
-  return `${ua.os.name} ${ua.os.version}`;
-});
-
-morgan.token("bot", function (req) {
-  const userAgent = req.headers["user-agent"];
-  const ua = uaParser(userAgent);
-  return ua.device.type === "bot" ? "yes" : "no";
-});
-
-// Define a custom morgan format to log JSON to the client
-morgan.format("json", function (tokens, req, res) {
-  if (req.headers["user-agent"] === "Consul Health Check") {
-    return;
-  }
-  const logLevel = res.statusCode >= 400 ? "error" : "info";
-  return `[${logLevel}] ${JSON.stringify(
-    {
-      ip: tokens["remote-addr"](req, res),
-      userAgent: req.headers["user-agent"],
-      browser: tokens.browser(req),
-      os: tokens.os(req),
-      isBot: tokens.bot(req),
-      method: tokens.method(req, res),
-      url: tokens.url(req, res),
-      status: tokens.status(req, res),
-      payload: JSON.stringify(req.body),
-      contentType: req.headers["content-type"],
-      responseTime: tokens["response-time"](req, res),
-    },
-    null,
-    2
-  )}`;
-});
+app.use(express.static("public")); // serve static files
 
 app.use(morgan("json")); // logs
 
