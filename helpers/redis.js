605c3340847d0475b448b3b64e83ee95b30ac1e9
Diego Fleitas <DiegoFleitas@users.noreply.github.com>
2023-03-24 00:18:13 -0300

    Redis connection pool (#10)
    * use redis connection pool
    
    * refactor pool
    
    * limit wait time when max conn reached

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,93 +1,39 @@
-const redis = require("redis");
 const crypto = require("crypto");
-
+const pool = require("./redisPool");
 require("dotenv").config();
 
-let redisClient = null;
-
 const isHealthy = async () => {
-  const client = await getRedisClient();
-  if (!client) {
-    return false;
-  }
   try {
-    const result = await client.ping();
+    const result = await pool.sendCommand("PING");
     return result === "PONG";
   } catch (error) {
-    console.log(error);
     return false;
   }
 };
 
-// create a Redis client instance if it doesn't exist, or return the existing one
-const getRedisClient = async () => {
-  if (!redisClient) {
-    try {
-      const options = {
-        url: process.env.FLYIO_REDIS_URL || "redis://localhost:6379",
-      };
-      console.log(options);
-      redisClient = redis
-        .createClient(options)
-        .on("error", (err) => {
-          console.log("Redis connection error", err);
-        })
-        .on("connect", () => {
-          console.log("Connected to Redis");
-        });
-      await redisClient.connect();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-  return redisClient;
-};
-
-// get a value from Redis cache
 const getCacheValue = async (key) => {
-  const client = await getRedisClient();
-  if (!client) {
-    return null;
-  }
   try {
     const hashedKey = getCacheKey(key);
-    const value = await client.get(hashedKey);
-    console.log(
-      `[${new Date().toISOString()}] Get cache value for key ${hashedKey} (${key})`
-    );
+    const value = await pool.get(hashedKey);
+
     try {
       return JSON.parse(value);
     } catch (error) {
       return value;
     }
   } catch (error) {
-    console.log(
-      `[${new Date().toISOString()}] Error getting cache value for key (${key})`
-    );
-    console.log(error);
     return null;
   }
 };
 
-// set a value in Redis cache with a TTL (time-to-live) in minutes
 const setCacheValue = async (key, value, ttl = 60) => {
-  const client = await getRedisClient();
-  if (!client) {
-    return null;
-  }
   try {
     const serializedValue = JSON.stringify(value);
     const hashedKey = getCacheKey(key);
-    const result = await client.set(hashedKey, serializedValue, { EX: ttl });
-    console.log(
-      `[${new Date().toISOString()}] Set cache value for key ${hashedKey} (${key}) with TTL ${ttl} min`
-    );
+    const result = await pool.set(hashedKey, serializedValue, ttl);
     return result === "OK";
   } catch (error) {
-    console.log(
-      `[${new Date().toISOString()}] Error setting cache value for key (${key}) with TTL ${ttl} min`
-    );
-    console.log(error);
+    return false;
   }
 };
 
