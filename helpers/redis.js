7814ff756b18263b6c7dfb95e223864fc25375b9
Diego Fleitas <diego.fleitas68@gmail.com>
2023-03-24 00:10:49 -0300

    refactor pool

--- helpers/redis.js
+++ helpers/redis.js
@@ -1,90 +1,39 @@
 const crypto = require("crypto");
-const redisConnectionPoolFactory = require("redis-connection-pool").default;
-
+const pool = require("./redisPool");
 require("dotenv").config();
 
-const REDIS_POOL_NAME = "default";
-
-const redisConfig = {
-  url: process.env.FLYIO_REDIS_URL || "redis://localhost:6379",
-  max_clients: 25,
-  perform_checks: true,
-  database: 0,
-};
-
-let pool;
-
-// use connection pool instead of creating a new redisPool for each request (performance)
-if (!pool) {
-  const createRedisPool = async () => {
-    return await redisConnectionPoolFactory(REDIS_POOL_NAME, redisConfig);
-  };
-  pool = createRedisPool();
-}
-
 const isHealthy = async () => {
-  const redisPool = await getRedisPool();
-  if (!redisPool) {
-    return false;
-  }
   try {
-    const result = await redisPool.sendCommand("PING");
+    const result = await pool.sendCommand("PING");
     return result === "PONG";
   } catch (error) {
-    console.log(error);
     return false;
   }
 };
 
-const getRedisPool = async () => {
-  return pool;
-};
-
-// get a value from Redis cache
 const getCacheValue = async (key) => {
-  const redisPool = await getRedisPool();
-  if (!redisPool) {
-    return null;
-  }
   try {
     const hashedKey = getCacheKey(key);
-    const value = await redisPool.get(hashedKey);
-    console.log(
-      `[${new Date().toISOString()}] Get cache value for key ${hashedKey} (${key})`
-    );
+    const value = await pool.get(hashedKey);
+
     try {
       return JSON.parse(value);
     } catch (error) {
       return value;
     }
   } catch (error) {
-    console.log(
-      `[${new Date().toISOString()}] Error getting cache value for key (${key})`
-    );
-    console.log(error);
     return null;
   }
 };
 
-// set a value in Redis cache with a TTL (time-to-live) in minutes
 const setCacheValue = async (key, value, ttl = 60) => {
-  const redisPool = await getRedisPool();
-  if (!redisPool) {
-    return null;
-  }
   try {
     const serializedValue = JSON.stringify(value);
     const hashedKey = getCacheKey(key);
-    const result = await redisPool.set(hashedKey, serializedValue, ttl);
-    console.log(
-      `[${new Date().toISOString()}] Set cache value for key ${hashedKey} (${key}) with TTL ${ttl} min`
-    );
+    const result = await pool.set(hashedKey, serializedValue, ttl);
     return result === "OK";
   } catch (error) {
-    console.log(
-      `[${new Date().toISOString()}] Error setting cache value for key (${key}) with TTL ${ttl} min`
-    );
-    console.log(error);
+    return false;
   }
 };
 
---
+++ helpers/redisPool.js
@@ -0,0 +1,15 @@
+const redisConnectionPoolFactory = require("redis-connection-pool").default;
+require("dotenv").config();
+
+const REDIS_POOL_NAME = "default";
+
+const redisConfig = {
+  url: process.env.FLYIO_REDIS_URL || "redis://localhost:6379",
+  max_clients: 25,
+  perform_checks: true,
+  database: 0,
+};
+
+const pool = redisConnectionPoolFactory(REDIS_POOL_NAME, redisConfig);
+
+module.exports = pool;
