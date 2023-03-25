e9a2273b945b61f6d3ee4119ba47ac1eb212a438
Diego Fleitas <DiegoFleitas@users.noreply.github.com>
2023-03-25 16:48:40 -0300

    refactor scripts (#13)

---
+++ public/showError.js
@@ -0,0 +1,27 @@
+export const showError = (error) => {
+  console.log(error);
+  // Check visible toast count before showing another toast
+  const toastCount =
+    document.querySelectorAll(".iziToast-capsule")?.length || 0;
+  if (toastCount >= 7) {
+    console.log(
+      `There are already ${toastCount} visible toasts on the page, error skipped. Message: ${error}`
+    );
+    return;
+  }
+  iziToast.show({
+    title: "Error",
+    message: error,
+    color: "red",
+    position: "topCenter",
+    progressBarColor: "red",
+    progressBarEasing: "linear",
+    progressBar: true,
+    timeout: 3000,
+    resetOnHover: true,
+    overlay: false,
+    overlayClose: true,
+    position: "topRight",
+    backgroundColor: "#fbc500",
+  });
+};
