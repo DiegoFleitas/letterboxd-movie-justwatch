5ecdb3b8da145218ea2833017e761a85c2953fc4
Diego Fleitas <diego.fleitas68@gmail.com>
2023-10-26 18:00:58 -0300

    use justwatch graphql api (#26)

--- controllers/searchMovie.js
+++ controllers/searchMovie.js
@@ -8,30 +8,31 @@ const PROXY = "";
 // TODO: refactor - way more complex than it needs to be
 export const searchMovie = async (req, res) => {
   try {
-    const { title, year, country } = req.body;
-    const countryCode = country || "es_UY";
+    const { title, year, country: countryCode } = req.body;
+    const [, country] = (countryCode || "es_UY").split("_");
+    const language = "en"; // hardcoded for now
 
     if (!title) {
       console.log("No movie title");
       return res.json({ message: "Movie not found", title: title, year: year });
     }
 
-    const cacheKey = `search-movie:${title}:${year}:${countryCode}`;
+    const cacheKey = `search-movie:${title}:${year}:${country}`;
     const cachedResponse = await getCacheValue(cacheKey);
+
     if (cachedResponse) {
       console.log("Response found (cached)");
       return res.json(cachedResponse);
     }
 
     const movieDbAPIKey = process.env.MOVIE_DB_API_KEY;
-
-    // Search for movie on MovieDB API
     let encodedTitle = encodeURIComponent(title);
     const movieDbResponse = await axios.get(
       `${PROXY}https://api.themoviedb.org/3/search/movie?query=${encodedTitle}${
         year ? `&year=${year}` : ""
       }&api_key=${movieDbAPIKey}`
     );
+
     const movieDbData = movieDbResponse.data.results[0];
 
     if (!movieDbData) {
@@ -40,34 +41,69 @@ export const searchMovie = async (req, res) => {
       return res.json(response);
     }
 
-    // Get title and year from MovieDB API
-    const movieId = movieDbData.id;
+    const tmdbId = movieDbData.id;
 
-    // Search for movie on JustWatch API using title and year
-    const titlesPromise = axios.get(
-      `${PROXY}https://api.justwatch.com/content/titles/${countryCode}/popular?body={"query": "${encodedTitle} ${year}"}`
+    const query = `
+      query GetSuggestedTitles($country: Country!, $language: Language!, $first: Int!, $filter: TitleFilter) {
+        popularTitles(country: $country, first: $first, filter: $filter) {
+          edges {
+            node {
+              id
+              objectType
+              objectId
+              content(country: $country, language: $language) {
+                fullPath
+                title
+                originalReleaseYear
+                posterUrl
+                fullPath
+                scoring {
+                  imdbScore
+                  __typename
+                }
+                externalIds {
+                  imdbId
+                  tmdbId
+                  __typename
+                }
+                __typename
+              }
+              offers(country: $country, platform: WEB) {
+                monetizationType
+                availableToTime
+                availableFromTime
+                standardWebURL
+                package {
+                  clearName
+                  technicalName
+                  icon
+                }
+              }
+              __typename
+            }
+            __typename
+          }
+          __typename
+        }
+      }
+    `;
+
+    const variables = {
+      country: country,
+      language: language,
+      first: 4,
+      filter: { searchQuery: title },
+    };
+
+    const justWatchResponse = await axios.post(
+      `${PROXY}https://apis.justwatch.com/graphql`,
+      { query, variables }
     );
 
-    // Get providers list from JustWatch API
-    const providersPromise = getProviders(countryCode);
-
-    // parallelize requests
-    const [justWatchResponse, providers] = await Promise.all([
-      titlesPromise,
-      providersPromise,
-    ]);
-
-    // Search for movie data in the JustWatch response based on the movie ID from MovieDB API
-    // This is done to filter out movies JustWatch "suggests" but are not necessarily the movie we are looking for
-    // TODO: condsider using original_release_year & object_type=movie ?
-    const movieData =
-      justWatchResponse?.data?.items.find((item) => {
-        // Unreleased movies might not have scoring
-        const tmdbId = item.scoring?.find(
-          (score) => score.provider_type === "tmdb:id"
-        );
-        return tmdbId && tmdbId.value === movieId;
-      }) || null;
+    const edges = justWatchResponse.data.data.popularTitles.edges;
+    const movieData = edges.find(
+      (edge) => edge.node.content.externalIds.tmdbId == tmdbId
+    );
 
     if (!movieData) {
       const response = { error: "Movie not found", title: title, year: year };
@@ -82,75 +118,30 @@ export const searchMovie = async (req, res) => {
       year,
     };
 
-    if (!movieData.offers || !movieData.offers.length) {
+    if (!movieData.node.offers || !movieData.node.offers.length) {
       await setCacheValue(cacheKey, noStreamingServicesResponse, cacheTtl);
       return res.json(noStreamingServicesResponse);
     }
 
-    let { streamingServices, directUrls } = movieData.offers.reduce(
-      (acc, offer) => {
-        if (
-          offer.monetization_type === "flatrate" ||
-          offer.monetization_type === "free" ||
-          offer.monetization_type === "ads"
-        ) {
-          let id = offer.provider_id
-          acc.streamingServices.push(id);
-
-          acc.directUrls[id] = offer?.urls?.standard_web;
-        }
-
-        return acc;
-      },
-      { streamingServices: [], directUrls: {} }
+    const providers = processOffers(
+      movieData.node.offers,
+      movieData.node.content.fullPath
     );
 
-    streamingServices = [...new Set(streamingServices)];
-
-    if (!streamingServices?.length) {
+    if (!providers?.length) {
       await setCacheValue(cacheKey, noStreamingServicesResponse, cacheTtl);
       return res.json(noStreamingServicesResponse);
     }
 
-    // use reduce to avoid iterating twice
-    const { clearNames, movieProviders } = streamingServices.reduce(
-      (acc, service) => {
-        const provider = providers.find((provider) => provider.id === service);
-        if (provider) {
-          acc.clearNames.push(provider.clear_name);
-          const icon = provider.icon_url.replace("{profile}", "");
-          acc.movieProviders.push({
-            id: service,
-            name: provider.clear_name,
-            icon: `https://www.justwatch.com/images${icon}s100/icon.webp`,
-            url: directUrls[service],
-          });
-        }
-        return acc;
-      },
-      { clearNames: [], movieProviders: [] }
-    );
-
-    if (!clearNames || !clearNames.length) {
-      const services = streamingServices.join(", ");
-      const response = {
-        error: `Unable to identify providers offering media. Provider id(s): ${services} (JustWatch)`,
-        title: title,
-        year: year,
-      };
-      console.log(response);
-      await setCacheValue(cacheKey, response, cacheTtl);
-      return res.json(response);
-    }
-
-    const response = {
+    const responsePayload = {
       message: "Movie found",
-      movieProviders: movieProviders,
-      title: title,
-      year: year,
+      movieProviders: providers,
+      title: movieData.node.content.title,
+      year: movieData.node.content.originalReleaseYear,
     };
-    await setCacheValue(cacheKey, response, cacheTtl);
-    res.json(response);
+
+    await setCacheValue(cacheKey, responsePayload, cacheTtl);
+    res.json(responsePayload);
   } catch (err) {
     console.log(err);
     res
@@ -159,8 +150,31 @@ export const searchMovie = async (req, res) => {
   }
 };
 
-const getProviders = async (countryCode) => {
-  const cacheKey = `justwatch-providers:${countryCode}`;
+const processOffers = (offers, fullPath) => {
+  const uniqueProviders = new Set(); // Set to store unique serialized providers
+  return offers.reduce((acc, offer) => {
+    if (["FLATRATE", "FREE", "ADS"].includes(offer.monetizationType)) {
+      const provider = {
+        id: offer.package.technicalName,
+        name: offer.package.clearName,
+        icon: `https://images.justwatch.com${offer.package.icon
+          .replace("{profile}", "s100")
+          .replace("{format}", "jpg")}`,
+        url: offer.standardWebURL || fullPath,
+        type: offer.monetizationType,
+      };
+      const serializedProvider = JSON.stringify(provider);
+      if (!uniqueProviders.has(serializedProvider)) {
+        uniqueProviders.add(serializedProvider); // Add serialized provider to Set
+        acc.push(provider); // Push provider object to accumulator
+      }
+    }
+    return acc;
+  }, []);
+};
+
+const getProviders = async (country) => {
+  const cacheKey = `justwatch-providers:${country}`;
   const cachedProviders = await getCacheValue(cacheKey);
 
   if (cachedProviders) {
@@ -168,20 +182,41 @@ const getProviders = async (countryCode) => {
   }
 
   try {
-    const providersResponse = await axios.get(
-      `${PROXY}https://apis.justwatch.com/content/providers/locale/${countryCode}`
+    const query = `
+      query GetPackages($platform: Platform! = WEB, $country: Country!) {
+        packages(country: $country, platform: $platform, includeAddons: false) {
+          clearName
+          id
+          shortName
+          technicalName
+          packageId
+          selected
+          monetizationTypes
+          __typename
+        }
+      }
+    `;
+
+    const data = {
+      query,
+      variables: { platform: "WEB", country: country },
+    };
+
+    const providersResponse = await axios.post(
+      "https://apis.justwatch.com/graphql",
+      data
     );
-    const providers = providersResponse.data;
-    providers?.map((elem) => {
-      const { id, clear_name } = elem;
-      return { id, clear_name };
-    }) || [];
-    // cache for 1 day since providers don't change often
-    const cacheTtl = 60 * 60 * 24 * 1;
+
+    const providers = providersResponse.data.data.packages.map((provider) => ({
+      id: provider.id,
+      clear_name: provider.clearName,
+    }));
+
+    const cacheTtl = 60 * 60 * 24 * 1; // cache for 1 day since providers don't change often
     await setCacheValue(cacheKey, providers, cacheTtl);
     return providers;
   } catch (error) {
-    console.error(`Error fetching providers for ${countryCode}:`, error);
+    console.error(`Error fetching providers for ${country}:`, error);
     throw error;
   }
 };
