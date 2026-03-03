605529c94fc65cccb47ea5698053a04d1535d739
Diego Fleitas <diego.fleitas68@gmail.com>
2026-03-03 16:51:55 -0300

    fix: remove specific eslint disable comment for clarity

--- public/src/vite-env.js
+++ public/src/vite-env.js
@@ -2,7 +2,7 @@
 // Intentionally minimal so that any stale imports of "./vite-env.js" resolve
 // without breaking the dev server or Playwright E2E runs.
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// eslint-disable-next-line
 // @ts-ignore
 window.__CANONICAL_PROVIDERS_BY_NAME__ = window.__CANONICAL_PROVIDERS_BY_NAME__ || {};
 
